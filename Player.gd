390daab25f616fbb2a33c7f0a4607a3b67d77a5a
Berk-CanAtesoglu <74148320+Berk-CanAtesoglu@users.noreply.github.com>
2022-04-22 18:44:40 +0200

    First_push

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export (int) var speed
+var motion = Vector2(0, 0)
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	if Input. is_action_pressed("ui_up"):
+		motion.y -= speed * delta
+	elif Input.is_action_pressed("ui_down"):
+		motion.y += speed * delta
+	elif Input.is_action_pressed("ui_left"):
+		motion.x -= speed * delta
+	elif Input.is_action_pressed("ui_right"):
+		motion.x += speed * delta
+	else:
+		motion.x = 0
+		motion.y = 0
+		
+	motion = move_and_slide(motion)
+		
+	
+	
